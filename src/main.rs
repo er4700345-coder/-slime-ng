e470d673319095fa86813adee2522f737b00df86
Divine Sunday OFUOWOICHO <er4700345@gmail.com>
2026-03-25 19:05:57 +0100

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,14 @@
 use slime::lexer::Lexer;
 use slime::parser::Parser;
+use slime::typechecker::TypeChecker;
 
 fn main() {
     let input = r#"
-target wasm {
-    dom = true
-}
-
 fn main() {
-    let x = 42;
-    let msg = "Hello SLIME!";
-    print(msg);
-    return x;
+    let x: i32 = 42;
+    let y = "hello";
+    let z = x + 10;
+    let bad = x + y;
 }
 "#;
     
@@ -19,7 +16,12 @@ fn main() {
     let mut parser = Parser::new(lexer);
     let ast = parser.parse();
     
-    for decl in ast {
-        println!("{:#?}", decl);
+    let mut checker = TypeChecker::new();
+    checker.check_program(&ast);
+    
+    if checker.has_errors() {
+        checker.report_errors();
+    } else {
+        println!("Type check passed!");
     }
 }

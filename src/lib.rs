845ef0049b1d8ce74576480962890c08e405eb3b
Divine Sunday OFUOWOICHO <er4700345@gmail.com>
2026-03-26 08:19:28 +0100

    chore(lib): register native module

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,9 @@
-// src/lib.rs
 pub mod ast;
 pub mod error;
 pub mod lexer;
 pub mod parser;
-pub mod stdlib;       // ← new
+pub mod stdlib;
 pub mod typechecker;
 pub mod wasm;
 pub mod wasm_binary;
+pub mod native; 

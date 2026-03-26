2c5ea7eaf51cf42f134df5dca9b5930f3047df52
Divine Sunday OFUOWOICHO <er4700345@gmail.com>
2026-03-26 16:29:02 +0100

    feat(pkg): add install command stub

--- src/registry.rs
+++ src/registry.rs
@@ -2,3 +2,8 @@ pub fn fetch_package(package_name: &str) {
     println!("Fetching package: {}", package_name);
     println!("Registry support is not fully implemented yet.");
 }
+
+pub fn install_all() {
+    println!("Installing dependencies from slime.toml...");
+    println!("Dependency installation is not fully implemented yet.");
+}

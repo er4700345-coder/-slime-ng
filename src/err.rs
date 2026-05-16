90bc17c6b4f5878b693854c82602b80352a92795
Divine Sunday OFUOWOICHO <er4700345@gmail.com>
2026-05-16 13:26:29 +0100

    feat(err): add SourceLocation and structured error variants with line/col

--- src/err.rs
+++ src/err.rs
@@ -1,19 +1,31 @@
 use std::fmt;
 
 #[derive(Debug, Clone)]
+pub struct SourceLocation {
+    pub line: usize,
+    pub col: usize,
+}
+
+#[derive(Debug, Clone)]
 pub enum SlimeError {
-    LexError(String),
-    ParseError(String),
-    TypeError(String),
+    LexError { msg: String, loc: SourceLocation },
+    ParseError { msg: String, loc: SourceLocation },
+    TypeError { msg: String, loc: Option<SourceLocation> },
     CodegenError(String),
 }
 
 impl fmt::Display for SlimeError {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         match self {
-            SlimeError::LexError(msg) => write!(f, "Lex error: {}", msg),
-            SlimeError::ParseError(msg) => write!(f, "Parse error: {}", msg),
-            SlimeError::TypeError(msg) => write!(f, "Type error: {}", msg),
+            SlimeError::LexError { msg, loc } => write!(f, "Lex error at {}:{}: {}", loc.line, loc.col, msg),
+            SlimeError::ParseError { msg, loc } => write!(f, "Parse error at {}:{}: {}", loc.line, loc.col, msg),
+            SlimeError::TypeError { msg, loc } => {
+                if let Some(l) = loc {
+                    write!(f, "Type error at {}:{}: {}", l.line, l.col, msg)
+                } else {
+                    write!(f, "Type error: {}", msg)
+                }
+            }
             SlimeError::CodegenError(msg) => write!(f, "Codegen error: {}", msg),
         }
     }
